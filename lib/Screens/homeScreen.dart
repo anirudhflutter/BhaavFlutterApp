abe8df81403b0d64ca0b5c7494fa28670e7c680c
ItFuturzTech9Teen <jagannathtech9teen@gmail.com>
2020-12-21 20:25:31 +0530

    All Api Work By Anirudh

--- lib/Screens/homeScreen.dart
+++ lib/Screens/homeScreen.dart
@@ -180,7 +180,6 @@ class _HomeScreenState extends State<HomeScreen> {
               isLoading = false;
             });
           } else {
-            // showMsg(data["Message"]);
           }
         }, onError: (e) {
           showMsg("Try Again.");
